923b0d56dd4a9ba0f456e7ea10dccf6b35755f45
yossyoss <yosi0679@gmail.com>
2019-03-01 09:35:12 +0200

    discover page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,24 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'auth', pathMatch: 'full' },
-  { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
-  { path: 'places', loadChildren: './places/places.module#PlacesPageModule' },
-  { path: 'discover', loadChildren: './places/discover/discover.module#DiscoverPageModule' },
-  { path: 'offers', loadChildren: './places/offers/offers.module#OffersPageModule' },
-  { path: 'new-offer', loadChildren: './places/offers/new-offer/new-offer.module#NewOfferPageModule' },
-  { path: 'edit-offer', loadChildren: './places/offers/edit-offer/edit-offer.module#EditOfferPageModule' },
-  { path: 'place-detail', loadChildren: './places/discover/place-detail/place-detail.module#PlaceDetailPageModule' },
-  { path: 'offer-booking', loadChildren: './places/offers/offer-booking/offer-booking.module#OfferBookingPageModule' },
-  { path: 'bookings', loadChildren: './bookings/bookings.module#BookingsPageModule' },
+  { path: "", redirectTo: "places", pathMatch: "full" },
+  { path: "auth", loadChildren: "./auth/auth.module#AuthPageModule" },
+  { path: "places", loadChildren: "./places/places.module#PlacesPageModule" },
+  {
+    path: "bookings",
+    loadChildren: "./bookings/bookings.module#BookingsPageModule"
+  }
+  // { path: 'discover', loadChildren: './places/discover/discover.module#DiscoverPageModule' },
+  // { path: 'offers', loadChildren: './places/offers/offers.module#OffersPageModule' },
+  // { path: 'new-offer', loadChildren: './places/offers/new-offer/new-offer.module#NewOfferPageModule' },
+  // { path: 'edit-offer', loadChildren: './places/offers/edit-offer/edit-offer.module#EditOfferPageModule' },
+  // { path: 'place-detail', loadChildren: './places/discover/place-detail/place-detail.module#PlaceDetailPageModule' },
+  // { path: 'offer-booking', loadChildren: './places/offers/offer-booking/offer-booking.module#OfferBookingPageModule' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { PlacesService } from "src/app/places.service";
+import { Place } from "src/app/place.model";
 
 @Component({
-  selector: 'app-discover',
-  templateUrl: './discover.page.html',
-  styleUrls: ['./discover.page.scss'],
+  selector: "app-discover",
+  templateUrl: "./discover.page.html",
+  styleUrls: ["./discover.page.scss"]
 })
 export class DiscoverPage implements OnInit {
-
-  constructor() { }
+  LoadedPlaces: Place[];
+  constructor(private placeService: PlacesService) {}
 
   ngOnInit() {
+    this.LoadedPlaces = this.placeService.places;
   }
-
 }
