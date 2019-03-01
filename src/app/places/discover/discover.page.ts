b8be44e5a48c7c58214a082d02107bd7f668b4f8
yossyoss <yosi0679@gmail.com>
2019-03-01 12:23:40 +0200

    adding complete side menu

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { PlacesService } from "src/app/places.service";
 import { Place } from "src/app/place.model";
+import { MenuController } from "@ionic/angular";
 
 @Component({
   selector: "app-discover",
@@ -9,9 +10,15 @@ import { Place } from "src/app/place.model";
 })
 export class DiscoverPage implements OnInit {
   LoadedPlaces: Place[];
-  constructor(private placeService: PlacesService) {}
+  constructor(
+    private placeService: PlacesService,
+    private menuCtrl: MenuController
+  ) {}
 
   ngOnInit() {
     this.LoadedPlaces = this.placeService.places;
+  }
+  onOpenMenu() {
+    this.menuCtrl.toggle();
   }
 }
